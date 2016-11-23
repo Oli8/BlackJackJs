16ffda0090a6c0f7e5089e704dbd77c312a5e35f
Olivier Crete <olivier.crete@orange.com>
2016-11-23 16:24:50 +0100

    background and build deck

--- js/conf.js
+++ js/conf.js
@@ -12,9 +12,28 @@ const imgs = {
 	}
 };
 
-const suits = ['Clubs', 'Diamonds', 'Heartds', 'Spades'];
+const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 var cards = [];
 for(let i=2; i<11; i++)
 	cards.push(i);
 
 cards.push('J', 'Q', 'K', 'A');
+
+class Card{
+
+	constructor(suit, value){
+		this.suit = suit;
+		this.value = value;
+	}
+
+}
+
+// var deck = [];
+// for(var suit of suits){
+// 	for(var i=2; i<11; i++)
+// 		deck.push(new Card(suit, i));
+	
+// 	for(var v of ['J', 'Q', 'K', 'A'])
+// 		deck.push(new Card(suit, v));
+// }
+
--- js/game.js
+++ js/game.js
@@ -1,4 +1,42 @@
 function init(){
 
 	var stage = new createjs.Stage("canvas");
+
+	var game = {
+
+		deck: [],
+
+		start: function(){
+			this.buildDeck();
+		},
+
+		buildDeck: function(){
+			for(var suit of suits){
+				for(var i=2; i<11; i++)
+					this.deck.push(new Card(suit, i));
+				
+				for(var v of ['J', 'Q', 'K', 'A'])
+					this.deck.push(new Card(suit, v));
+			}
+		},
+
+		deckValue: function(){
+
+		}
+
+	};
+
+	var bank = {
+
+		deck: [];
+
+	};
+
+	var player = {
+
+		deck: [];
+	};
+
+	game.start();
+	game.deck.forEach(v => console.log(v))
 }
